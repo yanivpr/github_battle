d4830eb43538ad4f24b732e3b2c90abd948516d6
yanivpr <yaniv.preiss@fyber.com>
2016-05-03 23:06:12 +0200

    dynamic loading component

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -6,6 +6,7 @@ var Link = ReactRouter.Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 function puke(object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -14,7 +15,7 @@ function puke(object) {
 function ConfirmBattle(props) {
   return(
     props.isLoading === true
-    ? <p> Loading! </p>
+    ? <Loading speed={800} text='Waiting'/>
     : <MainContainer>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
